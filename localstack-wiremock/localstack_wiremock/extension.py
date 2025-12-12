16ae4e8a33540812f1173e064b26d83fe2e660e4
Waldemar Hummer <waldemar.hummer@gmail.com>
2025-12-11 16:47:31 -0800

    add simple CI build for WireMock extension (#8)

--- localstack-wiremock/localstack_wiremock/extension.py
+++ localstack-wiremock/localstack_wiremock/extension.py
@@ -7,10 +7,13 @@ class WireMockExtension(ProxiedDockerContainerExtension):
     HOST = "wiremock.<domain>"
     # name of the Docker image to spin up
     DOCKER_IMAGE = "wiremock/wiremock"
+    # name of the container
+    CONTAINER_NAME = "ls-wiremock"
 
     def __init__(self):
         super().__init__(
             image_name=self.DOCKER_IMAGE,
             container_ports=[8080],
+            container_name=self.CONTAINER_NAME,
             host=self.HOST,
         )
